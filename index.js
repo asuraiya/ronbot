8930d71a6f06ed78077d7e1e7fdfe08fcd403211
Abhinav Suraiya <absuraiya@gmail.com>
2016-12-03 17:29:16 -0600

    responds to the commands now

--- index.js
+++ index.js
@@ -35,6 +35,38 @@ controller.hears(['hello', 'hi'], ['direct_mention'], function (bot, message) {
   bot.reply(message, 'Hello.')
 })
 
+controller.hears(['add (.*) to the agenda',
+                  'add (.*) to the motw agenda',
+                  'add (.*) to the MOTW agenda'], 'direct_message,direct_mention,mention', function(bot, message) {
+    var agendaItem = message.match[1];
+    bot.reply(message, 'OK! Adding "'+ agendaItem +'" to the agenda for the next meeting,');
+});
+
+controller.hears(['MOTW agenda', 'motw agenda', "agenda?"], 'direct_message,direct_mention,mention', function(bot, message) {
+    bot.reply(message, "Here's the agenda:");
+});
+
+controller.hears(['motw', '/motw', 'add to the agenda'], 'direct_message,direct_mention,mention', function(bot, message) {
+    bot.startConversation(message, function(err, convo) {
+      if (!err) {
+            convo.ask('What should I add to the motw agenda?', function(response, convo) {
+                convo.next();
+            }, {'key': 'agenda'}); // store the results in a field called `agenda`
+
+            convo.on('end', function(convo) {
+                if (convo.status == 'completed') {
+                    agendaItem = convo.extractResponse('agenda');
+                    bot.reply(message, 'OK! Adding "'+ agendaItem +'" to the agenda for the next meeting,');
+                } else {
+                    // this happens if the conversation ended prematurely for some reason
+                    bot.reply(message, 'OK, nevermind!');
+                }
+            });
+        }
+    });
+});
+
+
 controller.hears(['hello', 'hi'], ['direct_message'], function (bot, message) {
   bot.reply(message, 'Hello.')
   bot.reply(message, 'It\'s nice to talk to you directly.')
